bffa5aa0134f0a9b9767a545729c4f8f4a2a4144
Timur Muhametjanov <timux73@gmail.com>
2022-04-28 19:51:42 +0400

    First Commit

---
+++ count.go
@@ -0,0 +1,15 @@
+﻿package main
+
+import (
+	"fmt"
+	"github.com/danny0012/datafile"
+	"log"
+)
+
+func main() {
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(lines)
+}
